8df931131034f76b943491caa9f382d2105870b6
Bruce Gordon <bgordon@focusls.com>
2017-10-27 14:56:07 -0400

    go

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, ApplicationRef } from '@angular/core';
+import {NgForm} from '@angular/forms';
 import {Model} from "./repository.model";
 import {Product} from "./product.model";
 
@@ -42,6 +43,26 @@ export class ProductComponent {
         }
       }
     }
+    return messages;
+  }
+
+  formSubmitted: boolean = false;
+
+  submitForm(form: NgForm) {
+    this.formSubmitted = true;
+    if(form.valid) {
+      this.addProduct(this.newProduct);
+      this.newProduct = new Product();
+      form.reset();
+      this.formSubmitted = false;
+    }
+  }
+
+  getFormValidationMessages(form: NgForm): string[] {
+    let messages: string[] = [];
+    Object.keys(form.controls).forEach(k => {
+      this.getValidationMessages(form.controls[k], k).forEach(m => messages.push(m));
+    });
     return messages;
   }
 }
